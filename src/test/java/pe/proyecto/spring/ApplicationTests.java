341d66b4136a37998c4f503a25b8193ad7714235
dchipana <dchipana@globokas.com>
2020-12-04 09:41:19 -0500

    first commit

---
+++ src/test/java/pe/proyecto/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.proyecto.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
